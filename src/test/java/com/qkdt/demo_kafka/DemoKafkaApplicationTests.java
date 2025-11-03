92ce03c7ba381fd921c49ffcbde0e0ca7b9a5867
xuannghi19 <xuannghi19t@outlook.com>
2025-11-03 16:00:29 +0700

    first commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/qkdt/demo_kafka/DemoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qkdt.demo_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
